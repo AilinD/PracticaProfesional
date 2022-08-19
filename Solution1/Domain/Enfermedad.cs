5a90f1b8031f7cbc848d148bcbf7b49af578b17b
Ailin Diego <ailindiego@gmail.com>
2022-08-18 23:23:17 -0300

    Se agregan las clases de las BLL's a la capa de negocio

---
+++ Solution1/Domain/Enfermedad.cs
@@ -0,0 +1,17 @@
+﻿using DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Enfermedad
+    {
+        public Guid ID { get; set; }
+        public string Descripcion { get; set; }
+
+        public List<Sintoma> sintomas { get; set; }
+    }
+}
