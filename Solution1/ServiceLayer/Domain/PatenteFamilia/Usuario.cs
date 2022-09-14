09d96dd70e94cc475a337d7894cf02072df98e0f
AilinD <ailindiego@gmail.com>
2022-09-13 23:59:35 -0300

    se borra la referencia .dll que corrompia el servicelayer en el .csproj

--- Solution1/ServiceLayer/Domain/PatenteFamilia/Usuario.cs
+++ Solution1/ServiceLayer/Domain/PatenteFamilia/Usuario.cs
@@ -1,11 +1,10 @@
-﻿//using ServiceLayer.Servicios.Criptografía;
+﻿
+
+using ServiceLayer.Domain.PatenteFamilia;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ServiceLayer.Domain.PatenteFamilia
+namespace ServiceLayer.PatenteFamilia
 {
 	public class Usuario
 	{
