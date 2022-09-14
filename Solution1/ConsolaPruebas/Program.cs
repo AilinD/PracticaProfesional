a6fcb453eb37372a23edfb9ff7f6bd490eb046d0
AilinD <ailindiego@gmail.com>
2022-09-14 11:10:11 -0300

    prueba2

--- Solution1/ConsolaPruebas/Program.cs
+++ Solution1/ConsolaPruebas/Program.cs
@@ -52,7 +52,7 @@ namespace ConsolaPruebas
             //Patente_dal.SelectAll();
             //Familia_dal.SelectAll();
 
-            Console.WriteLine("NuevaPrueba");
+            Console.WriteLine("BusBus");
    
             Console.ReadKey();   
         }
